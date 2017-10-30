d271897d2891cebd0939bad27a8b8a95f9842111
Nathan Haberman <na.haberman@gmail.com>
2017-10-30 14:54:36 -0500

    Added Boxing/Unboxing

---
+++ Language_Essentials/Boxing_Unboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> myList = new List<object>();
+            myList.Add(7);
+            myList.Add(28);
+            myList.Add(-1);
+            myList.Add(true);
+            myList.Add("chair");
+
+            int sum = 0;
+            foreach(object element in myList){
+                System.Console.WriteLine(element);
+                if (element is int){
+                    sum += Convert.ToInt32(element);
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
